85ce26c360678ddb8df032ebdcadb17f54ee9def
JAVIER <javiermonjesnuevo@gmail.com>
2024-09-22 23:33:11 -0600

    added exam files

---
+++ Python/print.py
@@ -0,0 +1,16 @@
+if 5 > 2:
+  print("¡Cinco es mayor que dos!")
+
+
+
+
+if pwd == 'manzana':
+    print('Iniciando sesión ...')
+else:
+    print('Contraseña incorrecta.')
+
+print('¡Todo terminado!')
+
+
+
+
